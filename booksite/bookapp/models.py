3915412b0cc9a27a590826efc351d2c8888b6376
officialalkenes <belloabdulhakeemolamide@gmail.com>
2021-08-04 15:46:46 +0100

    created the category models and Book models

--- booksite/bookapp/models.py
+++ booksite/bookapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+#Creating a Models for Category
+
+class Category(models.Model):
+    name = models.CharField("Categories", max_length=60)
+    slug = models.SlugField(max_length=20,  blank=True, null=True)
+
+
+    def __str__(self):
+        return self.name
+    
+    
+class Books(models.Model):
+    title = models.CharField(max_length=80)
+    author = models.CharField(max_length=80)
+    genre = models.CharField(max_length=80)
+    description = models.TextField()
+    
